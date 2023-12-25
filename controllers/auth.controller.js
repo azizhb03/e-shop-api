1bab7758eb9112a38c710ba97323631393bf7400
azizhb03 <hbaziz03@gmail.com>
2023-12-25 12:53:37 +0100

    up

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,7 @@
 const bcrypt = require("bcrypt");
 const User = require("../models/auth.model");
+const jwt = require("jsonwebtoken");
+const fs = require("fs");
 
 const register = async (req, res) => {
   try {
@@ -21,7 +23,59 @@ const register = async (req, res) => {
     res.status(500).json({ message: e.message });
   }
 };
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
 
+    if (!user) {
+      res.status(404).json({ message: "not found" });
+    } else {
+
+      const match = bcrypt.compareSync(password, user.password);
+
+      if (!match)
+        res.status(400).json({ message: "please check your password !" });
+      else {
+        const privateKey = fs.readFileSync("./private-key.pem", "utf8");
+
+        const token = jwt.sign(
+          {
+            exp: Math.floor(Date.now() / 1000) + 60 * 60,
+            data: {
+              _id: user._id,
+              username: user.username,
+              email: user.email,
+              role: user.role,
+            },
+          },
+          privateKey,
+          { algorithm: "RS256" }
+        );
+
+        const exp = Date.now() + 1000 * 60 * 60 * 7;
+
+        return res.cookie("Authorization", token).status(200)
+          .send({
+            message: "login with success",
+            data: { email: user.email, id: user._id },
+          })
+      }
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ message: e.message });
+  }
+};
+const logout = (req, res) => {
+
+  res.clearCookie('Authorization');
+  res.send('cookie foo cleared');
+
+
+};
 module.exports = {
   register,
+  login,
+  logout,
 };
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,18 +1,24 @@
-const express = require('express')
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const router = express.Router();
+const fs = require("fs");
 
-const router = express.Router()
+router.post("/register", require("../controllers/auth.controller").register);
+router.post("/login", require("../controllers/auth.controller").login);
+router.get("/logout", require("../controllers/auth.controller").logout);
+router.get("/check", (req, res) => {
+    const privateKey = fs.readFileSync("./private-key.pem", "utf8");
+    jwt.verify(req.query.token, privateKey, function (err, decoded) {
+        if (decoded) {
+            res.status(200).json({ message: "isAuth" });
+        }
+        else if (err) {
+            res.status(401).json({ message: "session expired" });
 
+        }   
+    });
 
+}
+)
 
-
-
-
-
-
-router.post('/register' ,require('../controllers/auth.controller').register)
-
-
-
-
-
-module.exports = router
+module.exports = router;
